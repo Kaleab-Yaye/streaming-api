23b322d527b2a9a2ffa6072ceabbeedb844dc87a
Kaleab-Yaye <kaleabtesfaye222@gmail.com>
2025-09-03 21:05:21 +0300

    chore(tooling): add project scaffold, formatters, and pre-commit hooks

---
+++ src/main/java/com/adnakiwoch/platform/streaming_api/StreamingApiApplication.java
@@ -0,0 +1,12 @@
+package com.adnakiwoch.platform.streaming_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StreamingApiApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(StreamingApiApplication.class, args);
+  }
+}
